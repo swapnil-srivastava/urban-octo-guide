3fd89abb109d37126bf06b80ff55bcae8112b272
Swapnil Srivastava <swapnilsrivastava68@hotmail.com>
2024-09-07 03:27:19 +0000

    adding the basic sprint boot app

---
+++ src/main/java/com/greenplate/greenplate/GreenplateApplication.java
@@ -0,0 +1,13 @@
+package com.greenplate.greenplate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GreenplateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GreenplateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/greenplate/greenplate/GreenplateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.greenplate.greenplate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GreenplateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
