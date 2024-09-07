153584182e85bb6d17520d27ddec0502f161148d
Swapnil Srivastava <swapnilsrivastava68@hotmail.com>
2024-09-07 18:17:56 +0000

    adding the cors registry in the configuration

--- src/main/java/com/greenplate/greenplate/MyAutoConfiguration.java
+++ src/main/java/com/greenplate/greenplate/MyAutoConfiguration.java
@@ -1,9 +1,19 @@
 package com.greenplate.greenplate;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class MyAutoConfiguration {
+public class MyAutoConfiguration implements WebMvcConfigurer {
+    
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+            .allowedOrigins("https://curly-succotash-web.vercel.app")
+            .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+            .allowedHeaders("*")
+            .allowCredentials(true);
+    }
     
 }
-
