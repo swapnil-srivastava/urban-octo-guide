8f9e2f1c39ae88d75eb1f05b17bf08cdac461d2d
Swapnil Srivastava <swapnilsrivastava68@hotmail.com>
2024-09-07 18:29:21 +0000

    adding the @nonNull annotation

--- src/main/java/com/greenplate/greenplate/CorsConfig.java
+++ src/main/java/com/greenplate/greenplate/CorsConfig.java
@@ -1,6 +1,7 @@
 package com.greenplate.greenplate;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.lang.NonNull;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -8,7 +9,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class CorsConfig implements WebMvcConfigurer {
     
     @Override
-    public void addCorsMappings(CorsRegistry registry) {
+    public void addCorsMappings(@NonNull CorsRegistry registry) {
         registry.addMapping("/**")
             .allowedOrigins("https://curly-succotash-web.vercel.app")
             .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
