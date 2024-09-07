963fd1ce90e7a508e4a8432c867a800f5632e8fc
Swapnil Srivastava <swapnilsrivastava68@hotmail.com>
2024-09-07 21:31:25 +0000

    adding the ingredient model and ingridient repo

---
+++ src/main/java/com/greenplate/greenplate/repository/IngredientRepository.java
@@ -0,0 +1,9 @@
+package com.greenplate.greenplate.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.greenplate.greenplate.model.Ingredient;
+
+public interface IngredientRepository extends JpaRepository<Ingredient, Long> {
+    
+}
