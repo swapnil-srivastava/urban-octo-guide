f14c9939aeda330608bdb41f9ebec086dd8a0245
Swapnil Srivastava <swapnilsrivastava68@hotmail.com>
2024-09-08 00:37:42 +0000

    removing everything

--- src/main/java/com/greenplate/greenplate/repository/ChoiceRepository.java
+++
@@ -1,9 +0,0 @@
-package com.greenplate.greenplate.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.greenplate.greenplate.model.Choice;
-
-public interface ChoiceRepository extends JpaRepository<Choice, Long> {
-    
-}
