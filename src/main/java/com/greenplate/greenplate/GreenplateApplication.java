47c6472a1a76f944f8d8a96bfa0fdbb79f14c722
Swapnil Srivastava <swapnilsrivastava68@hotmail.com>
2024-09-30 11:57:35 +0000

    adding the logger

--- src/main/java/com/greenplate/greenplate/GreenplateApplication.java
+++ src/main/java/com/greenplate/greenplate/GreenplateApplication.java
@@ -1,5 +1,7 @@
 package com.greenplate.greenplate;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.client.RestTemplateBuilder;
@@ -9,6 +11,9 @@ import org.springframework.boot.CommandLineRunner;
 
 @SpringBootApplication
 public class GreenplateApplication {
+
+	private static final Logger log = LoggerFactory.getLogger(ConsumingRestApplication.class);
+
 	public static void main(String[] args) {
 		SpringApplication.run(GreenplateApplication.class, args);
 	}
@@ -23,7 +28,7 @@ public class GreenplateApplication {
 		return args -> { 
 			String url = "https://guess-a-number.azurewebsites.net/hack/0";
 			String response = restTemplate.getForObject(url, String.class);
-			System.out.println("response ::::::", response);
+			log.info("response ::::::>", response);
 		};
 	}
 
