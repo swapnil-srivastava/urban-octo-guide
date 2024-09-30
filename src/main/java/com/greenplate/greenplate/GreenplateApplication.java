255291eebf54528c09596ce24b8636d3786daa88
Swapnil Srivastava <swapnilsrivastava68@hotmail.com>
2024-09-30 12:36:19 +0000

    removing resttemplate bean

--- src/main/java/com/greenplate/greenplate/GreenplateApplication.java
+++ src/main/java/com/greenplate/greenplate/GreenplateApplication.java
@@ -1,13 +1,7 @@
 package com.greenplate.greenplate;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.client.RestTemplateBuilder;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.client.RestTemplate;
-import org.springframework.boot.CommandLineRunner;
 
 @SpringBootApplication
 public class GreenplateApplication {
@@ -17,9 +11,5 @@ public class GreenplateApplication {
 		SpringApplication.run(GreenplateApplication.class, args);
 	}
 
-	@Bean
-	public RestTemplate restTemplate(RestTemplateBuilder builder) {
-		return builder.build();
-	}
 
 }
