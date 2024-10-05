f4bbec97ca3ad5d5456ef3065f48f4a414b618ec
Swapnil Srivastava <swapnilsrivastava68@hotmail.com>
2024-10-05 20:08:22 +0000

    adding the logic to call gcp service using web client

--- src/main/java/com/greenplate/greenplate/controller/HelloWorldController.java
+++ src/main/java/com/greenplate/greenplate/controller/HelloWorldController.java
@@ -1,19 +1,25 @@
 package com.greenplate.greenplate.controller;
 
 import java.util.List;
+import java.time.Duration;
 import java.util.ArrayList;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
 
 import com.greenplate.greenplate.model.HelloWorldModel;
 import com.greenplate.greenplate.services.HelloWorldService;
+
+import reactor.core.publisher.Mono;
 
 @RestController
 @CrossOrigin(origins = "https://curly-succotash-web.vercel.app/")
@@ -24,6 +30,9 @@ public class HelloWorldController {
 
     @Autowired
     private RestTemplate restTemplate;
+
+    @Autowired
+    private WebClient webClient;
     
     @Value("${app.version}")
     private String appVersion;
@@ -60,15 +69,20 @@ public class HelloWorldController {
         return ResponseEntity.ok("Message from guess number service: " + message);
     }
 
-    @GetMapping("/call-gcp-service")
-    public ResponseEntity<String> callGcpService() {
-        String gcpService2Url = "https://swapnilsrivastava-eu-service-181557558593.europe-west3.run.app/message";
-        
-        ResponseEntity<String> response = restTemplate.getForEntity(gcpService2Url, String.class);
-        String message = response.getBody();
-        
-        return ResponseEntity.ok("Message from GCP Service: " + message);
-    }
+@GetMapping("/call-gcp-service")
+public Mono<ResponseEntity<String>> callGcpService() {
+    return webClient.get()
+        .uri("/message")
+        .retrieve()
+        .onStatus(HttpStatusCode::is4xxClientError, 
+            response -> Mono.error(new RuntimeException("4xx error")))
+        .onStatus(HttpStatusCode::is5xxServerError, 
+            response -> Mono.error(new RuntimeException("5xx error")))
+        .toEntity(String.class)
+        .timeout(Duration.ofSeconds(10));
+}
+
+
 
     @GetMapping("/load-list")
     public List<HelloWorldModel> getMethodName() {
