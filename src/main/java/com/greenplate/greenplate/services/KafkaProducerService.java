fcb38b84c2222c397f81b4c69ce2d11d7dfeac8c
Swapnil Srivastava <swapnilsrivastava68@hotmail.com>
2024-10-06 02:00:35 +0000

    adding the sendMessage for Kafka Producer service

---
+++ src/main/java/com/greenplate/greenplate/services/KafkaProducerService.java
@@ -0,0 +1,18 @@
+package com.greenplate.greenplate.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
+
+public class KafkaProducerService {
+    
+    private final KafkaTemplate<String, String> kafkaTemplate;
+
+    @Autowired
+    public KafkaProducerService(KafkaTemplate<String, String> kafkaTemplate) {
+        this.kafkaTemplate = kafkaTemplate;
+    }
+
+    public void sendMessage(String topic, String message) {
+        kafkaTemplate.send(topic, message);
+    }
+}
