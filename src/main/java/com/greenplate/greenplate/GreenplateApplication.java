2b3fabb0da30ae80c3d98f7f9ebe6fd8d3e595bd
Swapnil Srivastava <swapnilsrivastava68@hotmail.com>
2024-10-06 02:28:41 +0000

    adding enable kafka

--- src/main/java/com/greenplate/greenplate/GreenplateApplication.java
+++ src/main/java/com/greenplate/greenplate/GreenplateApplication.java
@@ -2,8 +2,10 @@ package com.greenplate.greenplate;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.kafka.annotation.EnableKafka;
 
 @SpringBootApplication
+@EnableKafka
 public class GreenplateApplication {
 
 
