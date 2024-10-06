1b35ae37bd270c6fcf303e55e8fcd1c516712efd
Swapnil Srivastava <swapnilsrivastava68@hotmail.com>
2024-10-06 02:35:01 +0000

    fixing the producer service by adding the service annotation

--- src/main/java/com/greenplate/greenplate/services/KafkaProducerService.java
+++ src/main/java/com/greenplate/greenplate/services/KafkaProducerService.java
@@ -2,17 +2,16 @@ package com.greenplate.greenplate.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
 
+@Service
 public class KafkaProducerService {
     
-    private final KafkaTemplate<String, String> kafkaTemplate;
-
     @Autowired
-    public KafkaProducerService(KafkaTemplate<String, String> kafkaTemplate) {
-        this.kafkaTemplate = kafkaTemplate;
-    }
+    private KafkaTemplate<String, String> kafkaTemplate;
 
     public void sendMessage(String topic, String message) {
         kafkaTemplate.send(topic, message);
     }
+    
 }
