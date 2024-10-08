c0829735dff22820ae73cd141466ba25f5468859
Swapnil Srivastava <swapnilsrivastava68@hotmail.com>
2024-10-08 00:08:30 +0000

    adding the kafka admin configuration and new topic builder

---
+++ src/main/java/com/greenplate/greenplate/KafkaTopicConfig.java
@@ -0,0 +1,34 @@
+package com.greenplate.greenplate;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.apache.kafka.clients.admin.AdminClientConfig;
+import org.apache.kafka.clients.admin.NewTopic;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.config.TopicBuilder;
+import org.springframework.kafka.core.KafkaAdmin;
+
+@Configuration
+public class KafkaTopicConfig {
+
+    @Value("${spring.kafka.bootstrap-servers}")
+    private String bootStrapServer;
+    
+    @Bean
+    public KafkaAdmin kafkaAdmin() {
+        Map<String, Object> configs = new HashMap<>();
+        configs.put(AdminClientConfig.BOOTSTRAP_SERVERS_CONFIG, bootStrapServer);
+        return new KafkaAdmin(configs);
+    }
+
+    @Bean
+    public NewTopic superManTopic() {
+        return TopicBuilder.name("superman-topic")
+                        .partitions(6)
+                        .replicas(3)
+                        .build();
+    }
+}
