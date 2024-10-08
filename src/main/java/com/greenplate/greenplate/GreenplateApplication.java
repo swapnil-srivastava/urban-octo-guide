92603170fc0ea674f1a5a96ece90d989be3ab72f
Swapnil Srivastava <swapnilsrivastava68@hotmail.com>
2024-10-08 13:26:31 +0000

    EnableKafkaStreams annotation used

--- src/main/java/com/greenplate/greenplate/GreenplateApplication.java
+++ src/main/java/com/greenplate/greenplate/GreenplateApplication.java
@@ -3,9 +3,11 @@ package com.greenplate.greenplate;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.kafka.annotation.EnableKafkaStreams;
 
 @SpringBootApplication
 @EnableKafka
+@EnableKafkaStreams
 public class GreenplateApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(GreenplateApplication.class, args);
