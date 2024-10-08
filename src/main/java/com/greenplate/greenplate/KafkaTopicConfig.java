d9eda71e7b711bdf86c51c8bf3ce5fde08dc0806
Swapnil Srivastava <swapnilsrivastava68@hotmail.com>
2024-10-08 19:19:26 +0000

    Addng the admin config using jaas config as well and create back to the future topic

--- src/main/java/com/greenplate/greenplate/KafkaTopicConfig.java
+++ src/main/java/com/greenplate/greenplate/KafkaTopicConfig.java
@@ -1,9 +1,11 @@
 package com.greenplate.greenplate;
 
+import java.util.HashMap;
 import java.util.Map;
 
 import org.apache.kafka.clients.admin.AdminClientConfig;
 import org.apache.kafka.clients.admin.NewTopic;
+import org.apache.kafka.common.config.SaslConfigs;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -13,26 +15,35 @@ import org.springframework.kafka.core.KafkaAdmin;
 @Configuration
 public class KafkaTopicConfig {
 
-    @Value("${spring.kafka.bootstrap-servers}")
-    private String bootstrapServers;
+    @Value("${spring.kafka.bootstrap-servers}") 
+    private String bootstrapAddress;
+  
+    @Value("${spring.kafka.properties.sasl.mechanism}") 
+      private String saslMechanism;
+    
+    
+    @Value("${spring.kafka.properties.sasl.jaas.config}") 
+      private String salsJaasConfig;
+  
+    @Value("${spring.kafka.properties.security.protocol}") 
+      private String securityProtocol;
 
-    @Bean 
-    public KafkaAdmin admin() {
-      return new KafkaAdmin(Map.of(AdminClientConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers));
+    @Value("${spring.kafka.properties.sasl.jaas.config}") 
+    private String saslJaasConfig;
+
+    @Bean
+    public KafkaAdmin kafkaAdmin() {
+        Map<String, Object> configs = new HashMap<>();
+        configs.put(AdminClientConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapAddress);
+        configs.put(AdminClientConfig.SECURITY_PROTOCOL_CONFIG, securityProtocol);
+        configs.put(SaslConfigs.SASL_MECHANISM, saslMechanism);
+        configs.put(SaslConfigs.SASL_JAAS_CONFIG, saslJaasConfig);
+        return new KafkaAdmin(configs);
     }
 
     @Bean
-    public NewTopic supermanTopic() {
-      return TopicBuilder.name("superman-topic")
-          .partitions(6)
-          .replicas(3)
-          .compact()
-          .build();
-    }
-
-    @Bean
-    public NewTopic streamsWordcountOutput() {
-      return TopicBuilder.name("streams-wordcount-output")
+    public NewTopic backToTheFuture() {
+      return TopicBuilder.name("back-to-the-future-topic")
           .partitions(6)
           .replicas(3)
           .compact()
